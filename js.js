58c9290f875206e1f780940a86e1d6d3460c2fcd
Your Name <shellzzonez@gmail.com>
2020-11-04 17:04:57 +0000

    js added event listeners and selectors and started func

--- js.js
+++ js.js
@@ -1,3 +1,20 @@
-* {
-    
+// Selectors
+const result = document.getElementsByClassName("result");
+const toss = document.getElementsByClassName("btn");
+const redo = document.getElementsByClassName("btn-2");
+
+//Event Listeners
+toss.addEventListener('click', tosscoin);
+redo.addEventListener('click', resetresult);
+
+
+//Functions
+
+function tosscoin(){
+
 }
+
+
+
+
+// function resetresult
