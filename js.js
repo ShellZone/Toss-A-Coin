3c4aa8e9c2119f78b369db207547d8743f53980f
Roshel Samarasinghe <samarasinghe.rt@googlemail.com>
2020-12-10 13:42:41 +0000

    added random num gen

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
-// Selectors
+// First we add selectors to select my 3 buttons and link them to variables
 const result = document.querySelector(".result");
 const toss = document.querySelector(".btn");
 const redo = document.querySelector(".btn-2");
@@ -6,19 +6,23 @@ const redo = document.querySelector(".btn-2");
 
 //Event Listeners
 toss.addEventListener("click", tosscoin);
+
+// added event listener to toss so that we can click , = what happens after we click === function 
+
 // redo.addEventListener("click", resetresult);
 
 
 //Functions
 
 function tosscoin(){
-    
-       
+    const coinValue = Math.floor(Math.random() * 2 + 1);
+    console.log(coinValue);
 }
 
 
 
 
-// function resetresult
+// function resetresult(){
 
-//test
+// }
+
